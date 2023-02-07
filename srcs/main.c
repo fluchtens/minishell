c62a4a0431242dae2524b8432fe0d9ed76fab8cc
Murillo Gomes De Senna <mgomes-d@mi-r7-p3.s19.be>
2023-02-07 10:34:57 +0100

    ceci est un test

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mgomes-d <mgomes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:00:23 by fluchten          #+#    #+#             */
-/*   Updated: 2023/02/07 10:16:22 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/02/07 10:34:00 by mgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int ac, char **av)
 {
 	(void)av;
 	if (ac == 1)
-		printf("in progress\n");
+		printf("in progrss\n");
 	else
 		return (print_error("Usage invalid (./minishell)"));
 }
