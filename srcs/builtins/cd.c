4507b27b7429b7c18fe0185a6d1b22e51d523a60
Cleazyy <cleazy2002@gmail.com>
2023-03-02 19:19:12 +0100

    opti cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 08:06:58 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/02 13:29:43 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:18:51 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,59 +41,37 @@ static void	update_pwd_envp(t_data *data)
 	}
 }
 
-static char	*get_path(t_data *data, t_cmds *cmds, char **err_msg, int *err_code)
+static int	path_perror(char *path)
+{
+	ft_putstr_fd("minishell: cd: ", 2);
+	perror(path);
+	free(path);
+	return (1);
+}
+
+int	ft_cd(t_data *data, t_cmds *cmds)
 {
 	char	*path;
+	int		ret;
 
-	path = NULL;
 	if (!cmds->str[1])
 	{
 		path = find_path(data, "HOME=");
 		if (!path)
-		{
-			*err_msg = ft_strdup("HOME");
-			*err_code = 1;
-		}
+			return (ft_print_error("minishell: cd: HOME not set"));
 	}
 	else if (ft_strncmp(cmds->str[1], "-", 1) == 0)
 	{
 		path = find_path(data, "OLDPWD=");
 		ft_putendl_fd(path, 1);
 		if (!path)
-		{
-			*err_msg = ft_strdup("OLDPWD");
-			*err_code = 1;
-		}
+			return (ft_print_error("minishell: cd: OLDPWD not set"));
 	}
 	else
 		path = ft_strdup(cmds->str[1]);
-	return (path);
-}
-
-int	ft_cd(t_data *data, t_cmds *cmds)
-{
-	char	*path;
-	char	*err_msg;
-	int		err_code;
-
-	err_code = 0;
-	err_msg = NULL;
-	path = get_path(data, cmds, &err_msg, &err_code);
-	if (err_code == 1)
-	{
-		ft_putstr_fd("minishell: cd: ", 2);
-		ft_putstr_fd(err_msg, 2);
-		ft_putendl_fd(" not set", 2);
-		free(err_msg);
-		return (1);
-	}
-	if (chdir(path) == -1)
-	{
-		ft_putstr_fd("minishell: cd: ", 2);
-		perror(path);
-		free(path);
-		return (1);
-	}
+	ret = chdir(path);
+	if (ret == -1)
+		return (path_perror(path));
 	free(path);
 	update_pwd(data);
 	update_pwd_envp(data);
