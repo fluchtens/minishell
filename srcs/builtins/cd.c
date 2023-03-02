219f1d5748407f7b2e208440ee6066d2f0813977
Cleazyy <cleazy2002@gmail.com>
2023-03-02 12:27:34 +0100

    up

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 08:06:58 by fluchten          #+#    #+#             */
-/*   Updated: 2023/02/28 18:44:01 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/02 12:27:20 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,17 +41,29 @@ static void	update_pwd_envp(t_data *data)
 	}
 }
 
-static char	*get_path(t_data *data, t_cmds *cmds)
+static char	*get_path(t_data *data, t_cmds *cmds, int *err_code, char **err_msg)
 {
 	char	*path;
 
 	path = NULL;
 	if (!cmds->str[1])
+	{
 		path = find_path(data, "HOME=");
+		if (!path)
+		{
+			*err_msg = ft_strdup("HOME");
+			*err_code = 1;
+		}
+	}
 	else if (ft_strncmp(cmds->str[1], "-", 1) == 0)
 	{
-		ft_putendl_fd(data->old_pwd, 1);
-		path = ft_strdup(data->old_pwd);
+		path = find_path(data, "OLDPWD=");
+		ft_putendl_fd(path, 1);
+		if (!path)
+		{
+			*err_msg = ft_strdup("OLDPWD");
+			*err_code = 1;
+		}
 	}
 	else
 		path = ft_strdup(cmds->str[1]);
@@ -61,11 +73,21 @@ static char	*get_path(t_data *data, t_cmds *cmds)
 int	ft_cd(t_data *data, t_cmds *cmds)
 {
 	char	*path;
+	char	*err_msg;
 	int		ret;
+	int		err_code;
 
-	path = get_path(data, cmds);
-	if (!path)
+	err_code = 0;
+	err_msg = NULL;
+	path = get_path(data, cmds, &err_code, &err_msg);
+	if (err_code)
+	{
+		ft_putstr_fd("minishell: cd: ", 2);
+		ft_putstr_fd(err_msg, 2);
+		ft_putendl_fd(" not set", 2);
+		free(err_msg);
 		return (1);
+	}
 	ret = chdir(path);
 	if (ret == -1)
 	{
