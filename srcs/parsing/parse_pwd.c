6dc040fddb367ff2e5fa9a299bdbcdbd55005823
Cleazyy <cleazy2002@gmail.com>
2023-03-02 19:27:31 +0100

    protect pwd parsing

--- srcs/parsing/parse_pwd.c
+++ srcs/parsing/parse_pwd.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 07:39:02 by fluchten          #+#    #+#             */
-/*   Updated: 2023/02/20 16:36:34 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:25:40 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	parse_pwd(t_data *data)
 	int	i;
 
 	i = 0;
+	data->pwd = NULL;
+	data->old_pwd = NULL;
 	while (data->envp[i])
 	{
 		if (!ft_strncmp(data->envp[i], "PWD=", 4))
@@ -30,7 +32,10 @@ int	parse_pwd(t_data *data)
 			len = ft_strlen(data->envp[i]) - 7;
 			data->old_pwd = ft_substr(data->envp[i], 7, len);
 		}
+		
 		i++;
 	}
+	if (!data->pwd || !data->old_pwd)
+		return (0);
 	return (1);
 }
