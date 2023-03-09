b3e772a74432155313967f47fc4b65ab1b39c921
Murillo Gomes De Senna <mgomes-d@shi-r8-p4.s19.be>
2023-03-09 10:54:17 +0100

    0 in expander

--- srcs/expander/expander_init.c
+++ srcs/expander/expander_init.c
@@ -6,13 +6,30 @@
 /*   By: mgomes-d <mgomes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 09:42:26 by mgomes-d          #+#    #+#             */
-/*   Updated: 2023/03/09 09:42:06 by mgomes-d         ###   ########.fr       */
+/*   Updated: 2023/03/09 10:52:49 by mgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	get_env_data(char **str, t_data *data, char **final)
+int	ft_return_value(char **final, char **temp)
+{
+	char	*finaltemp;
+	char	*num;
+
+	finaltemp = ft_strdup(*final);
+	if (*temp)
+		free(*temp);
+	if (*final)
+		free(*final);
+	num = ft_itoa(g_global.error_num);
+	*final = ft_strjoin(finaltemp, num);
+	free(finaltemp);
+	free(num);
+	return (2);
+}
+
+int	get_env_data(char **str, t_data *data, char **final, int j)
 {
 	char	*temp;
 	char	*path;
@@ -28,7 +45,7 @@ void	get_env_data(char **str, t_data *data, char **final)
 	free(path);
 	free(temp);
 	free(*str);
-	return ;
+	return (j + 1);
 }
 
 static char	*expander_utils(char *str, t_data *data)
@@ -45,12 +62,14 @@ static char	*expander_utils(char *str, t_data *data)
 		if (str[i] && str[i] == 36)
 		{
 			j = 0;
-			while (str[i + 1 + j] && !ft_iswhitespace(str[i + 1 + j]) \
-			&& str[i + 1 + j] != 39 && str[i + 1 + j] != 34)
+			while (str[i + 1 + j] != 34 && !ft_iswhitespace(str[i + 1 + j]) \
+			&& str[i + 1 + j] != 39 && str[i + 1 + j] && str[i + 1 + j] != 63)
 				j++;
 			temp = ft_substr(str, i + 1, j);
-			get_env_data(&temp, data, &final);
-			i += j + 1;
+			if (str[i + 1 + j] == 63 && j == 0)
+				i += ft_return_value(&final, &temp);
+			else
+				i += get_env_data(&temp, data, &final, j);
 		}
 		else
 			final = ft_strjoin_char(final, str[i++]);
@@ -71,5 +90,6 @@ void	init_expander(t_data *data, t_cmds *cmds)
 		cmds->str[i] = ft_remove_quotes(temp);
 		free(temp);
 	}
+	system("leaks minishell");
 	return ;
 }
