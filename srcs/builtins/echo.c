c53e016e89acce794ee82681c13a2a001e328496
Cleazyy <cleazy2002@gmail.com>
2023-03-10 10:00:38 +0100

    fix echo without args

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 08:09:45 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/08 09:47:52 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/10 09:59:27 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,14 @@ void	ft_echo(t_data *data, t_cmds *cmds)
 	int		is_n_flag;
 	int		i;
 
-	i = 1;
-	is_n_flag = 0;
 	(void) data;
+	if (!cmds->str[1])
+	{
+		ft_putchar_fd('\n', 1);
+		return ;
+	}
+	is_n_flag = 0;
+	i = 1;
 	if (cmds->str[1][0] == '-' && cmds->str[1][1] == 'n')
 	{
 		is_n_flag = 1;
