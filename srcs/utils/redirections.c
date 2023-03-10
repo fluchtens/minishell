4ece05331b0149d81a4f858cfd161e49dc7cb490
Murillo Gomes De Senna <mgomes-d@shi-r8-p4.s19.be>
2023-03-10 09:26:23 +0100

    fsanitize in redirection_add done

--- srcs/utils/redirections.c
+++ srcs/utils/redirections.c
@@ -3,25 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mgomes-d <mgomes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 07:33:47 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/09 10:45:43 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/10 09:25:00 by mgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	redirection_add(t_parser *parser, t_lexer *temp)
+static void	redirection_add(t_parser *parser, t_lexer **temp)
 {
 	t_lexer	*new;
 
-	new = lexer_new(ft_strdup(temp->next->str), temp->token);
+	new = lexer_new(ft_strdup((*temp)->next->str), (*temp)->token);
 	if (!new)
 		print_parser_error(ERR_MALLOC, parser->data, parser->lexer);
 	lexer_add_back(&parser->redirections, new);
-	lexer_delone(&parser->lexer, temp->i);
-	lexer_delone(&parser->lexer, temp->next->i);
+	lexer_delone(&parser->lexer, (*temp)->i);
+	lexer_delone(&parser->lexer, (*temp)->next->i);
+	*temp = (*temp)->next->next;
 }
 
 void	remove_redirections(t_parser *parser)
@@ -38,6 +39,6 @@ void	remove_redirections(t_parser *parser)
 	if (temp->next->token)
 		print_token_error(parser->data, parser->lexer, temp->next->token);
 	if ((temp->token >= GREAT && temp->token <= LESS_LESS))
-		redirection_add(parser, temp);
+		redirection_add(parser, &temp);
 	remove_redirections(parser);
 }
