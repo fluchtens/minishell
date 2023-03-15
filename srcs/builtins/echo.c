13325cba57246f458404a86c8b908069f2d4a1f4
Cleazyy <cleazy2002@gmail.com>
2023-03-15 08:29:30 +0100

    semi=fix echo with flags

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,11 +6,26 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 08:09:45 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/13 12:27:29 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/15 08:29:08 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	echo_nb_flags(char **str, int i, int *is_n_flag)
+{
+	while (str[i] && str[i][0] == '-')
+	{
+		if (ft_strncmp(str[i], "-n", 2))
+			break ;
+		else
+		{
+			(*is_n_flag) = 1;
+			i++;
+		}
+	}
+	return (i);
+}
 
 int	ft_echo(t_data *data, t_cmds *cmds)
 {
@@ -20,11 +35,8 @@ int	ft_echo(t_data *data, t_cmds *cmds)
 	(void) data;
 	is_n_flag = 0;
 	i = 1;
-	if (cmds->str[1] && cmds->str[1][0] == '-' && cmds->str[1][1] == 'n')
-	{
-		is_n_flag = 1;
-		i = 2;
-	}
+	if (ft_array_len(cmds->str) > 2)
+		i = echo_nb_flags(cmds->str, i, &is_n_flag);
 	while (cmds->str[i])
 	{
 		ft_putstr_fd(cmds->str[i], 1);
