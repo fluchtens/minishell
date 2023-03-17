d3cf3b4c2b1bbf23e0bdf84817d3e03b06c5089a
Cleazyy <cleazy2002@gmail.com>
2023-03-17 17:40:59 +0100

    protect if getcwd failed

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgomes-d <mgomes-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 09:15:50 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/17 07:30:37 by mgomes-d         ###   ########.fr       */
+/*   Updated: 2023/03/17 17:39:54 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int	ft_pwd(t_data *data, t_cmds *cmds)
 	(void) data;
 	(void) cmds;
 	temp = getcwd(NULL, sizeof(NULL));
+	if (!temp)
+		return (1);
 	ft_putendl_fd(temp, 1);
 	free(temp);
 	return (0);
