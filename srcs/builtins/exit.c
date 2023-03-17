91106a2294e0085c74260f893daa3a69c61c8a3b
Cleazyy <cleazy2002@gmail.com>
2023-03-17 17:26:09 +0100

    fix exit overflow

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,21 +6,31 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 11:58:46 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/08 10:24:56 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/17 17:25:14 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	exit_code(char **str)
+static long	exit_code(char **str)
 {
-	int	code;
+	long	code;
+	int		invalid;
 
 	code = 0;
 	if (!str[1])
 		code = 0;
 	else if (ft_str_isdigits(str[1]))
-		code = ft_atoi(str[1]);
+	{
+		code = ft_atol(str[1], &invalid);
+		if (invalid == 1)
+		{
+			ft_putstr_fd("minishell: exit: ", 2);
+			ft_putstr_fd(str[1], 2);
+			ft_putendl_fd(": numeric argument required", 2);
+			code = 255;
+		}
+	}
 	else if (!ft_str_isdigits(str[1]))
 	{
 		ft_putstr_fd("minishell: exit: ", 2);
@@ -33,7 +43,7 @@ static int	exit_code(char **str)
 
 int	ft_exit(t_data *data, t_cmds *cmds)
 {
-	int		code;
+	long	code;
 	int		args;
 
 	args = ft_array_len(cmds->str);
