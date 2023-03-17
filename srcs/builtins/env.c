787b69cc80fa48bbcc84c0f27abddf1460bb8b21
Cleazyy <cleazy2002@gmail.com>
2023-03-17 17:23:51 +0100

    test

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 09:02:05 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/13 12:15:59 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/17 17:23:45 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	ft_env(t_data *data, t_cmds *cmds)
 {
 	int		i;
 
-	i = 0;
 	(void) cmds;
+	i = 0;
 	while (data->envp[i])
 	{
 		ft_putendl_fd(data->envp[i], 1);
