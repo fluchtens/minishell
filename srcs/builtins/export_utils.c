fabd194d4fcb8060b261ecbb4ea5b70cd445c6f8
Cleazyy <cleazy2002@gmail.com>
2023-03-19 21:16:19 +0100

    fix export check cmd

--- srcs/builtins/export_utils.c
+++ srcs/builtins/export_utils.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 11:24:28 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/17 12:46:37 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/19 21:15:16 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,20 @@ static int	is_valid_export_cmd(char *str)
 	i = 0;
 	if ((!ft_isalpha(str[0]) && str[0] != '_'))
 		return (0);
-	while (str[i] && str[i] != '=')
+	while (str[i] && str[i] != '+' && str[i] != '=')
 	{
-		if (!ft_isalpha(str[i]) && !ft_isdigit(str[i])
-			&& str[i] != '_' && str[i] != '=' && str[i] != ' ')
+		if (!ft_isalpha(str[i]) && !ft_isdigit(str[i]) && str[i] != '_')
 			return (0);
 		i++;
 	}
-	if (!equal_pos(str))
-		return (0);
-	return (1);
+	if (str[i] == '=')
+		return (1);
+	else if (str[i] == '+')
+	{
+		if (str[i + 1] == '=')
+			return (1);
+	}
+	return (0);
 }
 
 int	check_export_cmd(char **str)
