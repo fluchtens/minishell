a028e41b0bc78508dcba8053de7aef6b2db1fc51
Cleazyy <cleazy2002@gmail.com>
2024-06-06 10:07:57 +0200

    fix builtins

--- srcs/builtins/builtins.c
+++ srcs/builtins/builtins.c
@@ -3,54 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fluchten <fluchten@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:16:34 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/17 07:40:10 by fluchten         ###   ########.fr       */
+/*   Updated: 2024/06/06 09:50:42 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	is_valid_command(char *builtin, char *str)
-{
-	int	builtinlen;
-	int	strlen;
-	int	len;
-
-	builtinlen = ft_strlen(builtin);
-	strlen = ft_strlen(str);
-	if (strlen < builtinlen)
-		len = builtinlen;
-	else
-		len = strlen;
-	if (ft_strncmp(str, builtin, len) == 0)
-		return (EXIT_SUCCESS);
-	return (EXIT_FAILURE);
-}
-
 int	(*get_builtin(char *str))(t_data *data, t_cmds *cmds)
 {
-	static void	*builtins[7][2] = {
-	{"echo", ft_echo},
-	{"cd", ft_cd},
-	{"pwd", ft_pwd},
-	{"export", ft_export},
-	{"unset", ft_unset},
-	{"env", ft_env},
-	{"exit", ft_exit}
-	};
-	int			i;
-
-	i = 0;
-	while (i < 7)
-	{
-		if (str)
-		{
-			if (!is_valid_command(builtins[i][0], str))
-				return (builtins[i][1]);
-		}
-		i++;
-	}
+	if (!str)
+		return (NULL);
+	else if (!ft_strcmp(str, "echo"))
+		return (ft_echo);
+	else if (!ft_strcmp(str, "cd"))
+		return (ft_cd);
+	else if (!ft_strcmp(str, "pwd"))
+		return (ft_pwd);
+	else if (!ft_strcmp(str, "export"))
+		return (ft_export);
+	else if (!ft_strcmp(str, "unset"))
+		return (ft_unset);
+	else if (!ft_strcmp(str, "env"))
+		return (ft_env);
+	else if (!ft_strcmp(str, "exit"))
+		return (ft_exit);
 	return (NULL);
 }
